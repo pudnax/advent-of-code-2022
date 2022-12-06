f86642b4e5713a1888b7f99dff57c13504ab16fa
Alex Komissarov <k.a.komissar@gmail.com>
2022-12-06 11:46:56 +0600

    Adjust impl

--- src/solutions/day06.rs
+++ src/solutions/day06.rs
@@ -7,33 +7,25 @@ use linkme::distributed_slice;
 
 const INPUT: &str = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/input/day6"));
 
+fn position_of_unique(s: &str, n: usize) -> Option<usize> {
+    s.as_bytes()
+        .windows(n)
+        .position(|arr| HashSet::<u8>::from_iter(arr.iter().copied()).len() == n)
+        .map(|n| n + n)
+}
+
 fn solve() -> Result<()> {
-    let input: Vec<char> = INPUT.chars().collect();
-    let mut res = 4;
-    for &arr in input.array_windows::<4>() {
-        let set = HashSet::from(arr);
-        if set.len() == 4 {
-            break;
-        }
-        res += 1;
-    }
+    let res = position_of_unique(INPUT, 4).unwrap_or(0);
     println!("Answer: {res}");
     Ok(())
 }
 
 fn solve2() -> Result<()> {
-    let input: Vec<char> = INPUT.chars().collect();
-    let mut res = 14;
-    for &arr in input.array_windows::<14>() {
-        let set = HashSet::from(arr);
-        if set.len() == 14 {
-            break;
-        }
-        res += 1;
-    }
+    let res = position_of_unique(INPUT, 14).unwrap_or(0);
     println!("Answer: {res}");
     Ok(())
 }
+
 #[distributed_slice(SOLUTIONS)]
 static SOLUTION_DAY_06_1: SolutionKey = (6, 1, solve);
 #[distributed_slice(SOLUTIONS)]
